1d0203d8a0697d1828fce050b5184a2e1f1c9321
Eduardo Franzoni <efdesigner@hotmail.com>
2020-01-05 00:27:49 -0300

    ajustes

---
+++ apps/ui/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Todo } from '@rapidmais/data';
+
+
+@Component({
+  selector: 'rapidmais-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ui';
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient){
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe(t => (this.todos = t));
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
+}
